b1ce9628ad458c581c12d72395dbe9d84f165e1e
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-04 21:34:34 +0530

    Added: `discord-promo-redeemer/src/settings/config.py`

---
+++ discord-promo-redeemer/src/settings/config.py
@@ -0,0 +1,28 @@
+DATABASE_NAME = "code_database.db"
+
+DISCORD_BOT_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+DISCORD_PREFIX = "!"
+
+KOOPECHKA_API_KEY = "YOUR_KOOPECHKA_API_KEY"
+FCAPTCHA_LOL_API_KEY = "YOUR_FCaptcha.lol_API_KEY"
+
+# Database Config
+DATABASE_CONFIG = {
+    "database_name": DATABASE_NAME
+}
+
+# Discord Bot Config
+DISCORD_BOT_CONFIG = {
+    "bot_token": DISCORD_BOT_TOKEN,
+    "bot_prefix": DISCORD_PREFIX
+}
+
+# Kopeechka API Config
+KOOPECHKA_API_CONFIG = {
+    "api_key": KOOPECHKA_API_KEY
+}
+
+# FCaptcha.lol API Config
+FCAPTCHA_LOL_API_CONFIG = {
+    "api_key": FCAPTCHA_LOL_API_KEY
+}
